acb0aecd81afc9aa877bb9da3f1b18313b5b2569
Rashad Bayram <rashadabayram@gmail.com>
2021-06-05 13:53:40 -0600

    added search result files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
-import { Editor } from "draft-js";
-import TextEditor from "./components/TextEditor";
+//import { Editor } from "draft-js";
+//import TextEditor from "./components/TextEditor";
 // import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import PostDialog from "./components/dialog_post"
 import TopNav from "./components/topNav";
+import SearchResult from "./components/SearchResult";
 
 export default function App() {
   return (
@@ -11,7 +12,9 @@ export default function App() {
       <TopNav/>
       <div className="postDialog"> 
       <PostDialog />
+      <SearchResult />
       </div>
+
       {/* <Router>
         <Link to="/postEditor" >Add a post</Link>
         <Route path="/postEditor" component={PostEditor}></Route>
---
+++ src/components/SearchResult.js
@@ -0,0 +1,11 @@
+import SingleSearch from './search/SingleSearch'
+import  "./search/all_search.scss"
+
+const SearchResult = () => {
+
+  return (
+    <SingleSearch />
+  )
+}
+
+export default SearchResult;
