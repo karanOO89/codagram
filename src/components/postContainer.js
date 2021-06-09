68e43d1e7932ff2dd303ac3aa0868a732a7f2322
karanOO89 <gk.987680@gmail.com>
2021-06-08 21:54:11 -0600

    fixed null image insertion

--- src/components/postContainer.js
+++ src/components/postContainer.js
@@ -1,7 +1,12 @@
 import PostBox from "./postBox";
 
 const PostContainer = (props) => {
+  // const [singlePost, setSinglePost] = useState({})
+  // props.postData.map(post => {
+  //   console.log(post)
+  //   setSinglePost({...post})
 
+  // })
   return (
     <div>      
       
